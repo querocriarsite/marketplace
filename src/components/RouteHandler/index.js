6fac97d18d3a3fa13642961bbb10d5443f635f1a
Everton Toffanetto <contato@querocriarsite.com>
2020-06-26 10:20:14 -0300

    Component RouteHandler add

---
+++ src/components/RouteHandler/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Route} from "react-router-dom";
+
+export default ({children, ...rest}) => {
+
+    return (
+        <Route
+            {...rest}
+            render={()=>children}
+        />
+    );
+};
